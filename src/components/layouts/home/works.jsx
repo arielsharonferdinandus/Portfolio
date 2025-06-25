32c60c0a7e052c3580d7b577b4c23d65a657b816
ArielSharonF <ariel.ferdinandus@cakrawala.ac.id>
2025-06-25 18:24:10 +0700

    Update works.jsx debugging div background color

--- src/components/layouts/home/works.jsx
+++ src/components/layouts/home/works.jsx
@@ -26,7 +26,7 @@ export default function Works({ showDay }) {
 
   return (
     <div
-      className={`relative flex flex-col justify-between items-center z-20 py-20 text-[#c6c6c6] font-micro transition-all duration-1000 ease-in-out
+      className={`relative flex flex-col justify-between items-center z-20 py-20 text-[#222] font-micro transition-all duration-1000 ease-in-out
         ${showDay ? '-top-1/7 sm:-top-1/3 pointer-events-auto' : 'top-9/10 pointer-events-auto'}`}
     >
       <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-9xl font-bold text-center -mb-2 sm:-mb-4 md:-mb-6 lg:-mb-8">MY PREV COMPANIES</h1>
@@ -36,7 +36,7 @@ export default function Works({ showDay }) {
         {companies.map((company, idx) => (
           <div
             key={idx}
-            className="flex flex-col items-center text-center bg-[#3c3c3c] border-t-4 border-r-4 border-b-4 border-l-4 border-t-[#585858] border-r-[#585858] border-b-[#1b1b1b] border-l-[#1b1b1b] p-2 sm:p-4 md:p-6 lg:p-8 rounded-s max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl w-full">
+            className="flex flex-col items-center text-center bg-[#c6c6c6] border-t-4 border-r-4 border-b-4 border-l-4 border-t-[#585858] border-r-[#585858] border-b-[#1b1b1b] border-l-[#1b1b1b] p-2 sm:p-4 md:p-6 lg:p-8 rounded-s max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl w-full">
             <img src={company.logo} alt={company.name} className="w-20 sm:w-40 h-20 sm:h-40 object-contain mb-4 overflow-hidden duration-300 hover:scale-125" />
             <h3 className="text-xs sm:text-xl md:text-2xl lg:text-5xl font-bold">{company.name}</h3>
             <h4 className="text-xs sm:text-xl md:text-2xl lg:text-3xl">{company.position}</h4>
